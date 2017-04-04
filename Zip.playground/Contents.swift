80e7b96847441a4da06c9e71df6582af0c3e52f9
Swift Funcional <info@swiftfuncional.com>
2017-04-04 08:47:32 +0200

    Use zip with the lists

--- Zip.playground/Contents.swift
+++ Zip.playground/Contents.swift
@@ -1,2 +1,8 @@
 let intNumbers = [1, 2, 3, 4, 5]
 let stringNumbers = ["one", "two", "three", "four", "five"]
+
+let zippedNumbers = zip(intNumbers, stringNumbers)
+
+for (int, string) in zippedNumbers {
+	print("\(string): \(int)")
+}
